ff45583a01db77114ee0026ce5cd59773510ea9b
psyitama <psyrone.agpalo@yahoo.com>
2021-06-08 21:50:04 +0800

    passed test

--- test/git.test.js
+++ test/git.test.js
@@ -57,8 +57,7 @@ describe('Testing add()', function () {
         git.init();
 
         let output_add = git.add('*');
-        // let output_status = git.status();
-        let output_status = null;
+        let output_status = git.status();
 
         expect(output_add).to.equal('Successfully added as index file/s.');
         expect(output_status).to.equal(
